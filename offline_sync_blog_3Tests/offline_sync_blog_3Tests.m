667afb811a0a06d34974ca53fbda31169cbae7a1
Tyler Casselman <tylerc230@gmail.com>
2012-03-28 17:31:59 -0700

    New tests for guid creation, and entity init state.

--- offline_sync_blog_3Tests/offline_sync_blog_3Tests.m
+++ offline_sync_blog_3Tests/offline_sync_blog_3Tests.m
@@ -27,6 +27,21 @@
 {
 	Post *newPost = [Post MR_createEntity];
 	STAssertNotNil(newPost.guid, @"GUID should be created when entity created");
+
+	Post *newPost2 = [Post MR_createEntity];
+	STAssertTrue(![newPost.guid isEqualToString: newPost2.guid], @"GUIDs should be unique");
+
+}
+
+- (void)testEntityInitialState
+{
+	Post *newPost = [Post MR_createEntity];
+	STAssertEquals((SyncObjectStatus)newPost.syncStatus, SONeedsSync, @"New entities should be in a needs sync state");
+
+	STAssertFalse(newPost.isGloballyDeleted, @"New entities should not be in a deleted state");
+
+	STAssertEquals((int)newPost.lastModified, 0, @"New entities should not have a last modified date");
+
 }
 
 @end
