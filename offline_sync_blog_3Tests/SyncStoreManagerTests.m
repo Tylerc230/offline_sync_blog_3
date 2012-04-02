dea73eac9df4ca32148980e5cafdf0f1f75ddfac
Tyler Casselman <tylerc230@gmail.com>
2012-04-02 12:08:52 -0700

    Basic push to server test passing
    This test ensures that once a new object has been pushed to the server,
    all its attributes are updated with the proper values (lastModifiedTime
    and syncStatus).

--- offline_sync_blog_3Tests/SyncStoreManagerTests.m
+++ offline_sync_blog_3Tests/SyncStoreManagerTests.m
@@ -7,28 +7,45 @@
 //
 
 #import "SyncStoreManagerTests.h"
+#import "OCMock.h"
 #import "ExampleSyncManager.h"
 #import "Post.h"
+@interface ExampleSyncManager (Test_methods)
+- (void)syncPayload:(NSDictionary *)payload;
+- (void)syncSucceededWithResponse:(id) responseObject;
+@end
 
 @implementation SyncStoreManagerTests
 - (void)setUp
 {
     [super setUp];
-    syncStorageManager_ = [[ExampleSyncManager alloc] initWithBaseURL:@"http://www.example.com"];
 }
 
 - (void)tearDown
 {
-	syncStorageManager_ = nil;
     [super tearDown];
 }
 
 - (void)testSync
 {
+	SyncStorageManager *storageManager = [[ExampleSyncManager alloc] initWithBaseURL:@"http://www.example.com"];
 	Post *newPost = [Post MR_createEntity];
-	[syncStorageManager_ syncNow];
 	
-	STAssertEquals(newPost.syncStatus, SOSynced, @"Entity should have a status of 'synced' after doing a sync");
+	NSMutableDictionary *mockEntity = [NSMutableDictionary dictionaryWithDictionary:[newPost toJson]];
+	[mockEntity setObject:[NSNumber numberWithDouble:[[NSDate date] timeIntervalSince1970]] forKey:kLastModifiedKey];
+	NSDictionary *mockServerResponse = [NSDictionary dictionaryWithObjectsAndKeys:[NSArray arrayWithObject:mockEntity], @"modifiedEntities", nil];
+
+	id storageMock = [OCMockObject partialMockForObject:storageManager];
+	[[[storageMock expect] andDo:^(NSInvocation *invocation){
+		[invocation.target syncSucceededWithResponse:mockServerResponse];
+	}] syncPayload:OCMOCK_ANY];
+
+	[storageMock syncNow];
+	
+	[[NSManagedObjectContext MR_contextForCurrentThread] refreshObject:newPost mergeChanges:YES];
+	SyncObjectStatus syncStatus = newPost.syncStatus;
+	
+	STAssertEquals(syncStatus, SOSynced, @"Entity should have a status of 'synced' after doing a sync");
 	
 	STAssertTrue(newPost.lastModified > 0, @"Entity last modified date should be set");
 					
