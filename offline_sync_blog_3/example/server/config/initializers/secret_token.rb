e94d823acddb3e42acd739fc63b1963734ce77a4
Tyler Casselman <tylerc230@gmail.com>
2012-04-12 16:09:19 -0700

    Adding new rails server to examples folder

---
+++ offline_sync_blog_3/example/server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Server::Application.config.secret_token = '4782b78c0ca9ddff44acdeddcf5514d82111c8a2944fd202743717437911be418ac44abffd552c616291f36fbefa2116d10df2349c3e40d4a1aaad91060c42b0'
