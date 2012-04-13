916ca7151d94c595fd800c783a38c528af6befb4
Tyler Casselman <tylerc230@gmail.com>
2012-04-12 18:57:38 -0700

    Server now handling json requests and server returning json in response

--- offline_sync_blog_3/example/server/app/controllers/sync_controller.rb
+++ offline_sync_blog_3/example/server/app/controllers/sync_controller.rb
@@ -1,5 +1,10 @@
 class SyncController < ApplicationController
   def sync
+    modifiedEntities = params[:modifiedEntities]
+    response = {}
+    respond_to do |format|
+          format.json { render :json => response}
+        end
   end
 
 end
