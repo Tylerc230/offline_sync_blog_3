b61d2635c60638f25318211b07d8005231e38401
Tyler Casselman <tylerc230@gmail.com>
2012-06-05 11:02:29 -0700

    Creating new sync_objects from the client tests passing
    When the client passes up newly created sync objects, created on
    the client, we are now successfully saving them to the db.

--- offline_sync_blog_3/example/server/app/helpers/sync_helper.rb
+++ offline_sync_blog_3/example/server/app/helpers/sync_helper.rb
@@ -1,11 +1,30 @@
 module SyncHelper
-  MODIFIED_ENTITIES_KEY = 'modifiedEntities'
-  LAST_SYNC_TIME_KEY = 'lastSyncTime'
+  MODIFIED_ENTITIES_KEY = :modified_entities
+  LAST_SYNC_TIME_KEY = :updated_at
+  GUID_KEY = :guid
+  CLASS_NAME_KEY = :classname
   def self.sync_entities client_modified_entities, client_last_sync
+    self.sync_client_modifications client_modified_entities
     response = {}
     server_modified_entities = SyncObject.modified_since client_last_sync
     response[MODIFIED_ENTITIES_KEY] = server_modified_entities
     response
+  end
+
+  def self.sync_client_modifications client_modified_entities
+    client_modified_entities.each do |entity|
+      klass = entity.keys.last
+      attributes = entity[klass]
+      guid = attributes[GUID_KEY]
+      class_name = attributes[CLASS_NAME_KEY]
+      updated_record = SyncObject.find_by_guid guid
+      if updated_record.nil?
+        updated_record = klass.to_s.capitalize.constantize.new
+      end
+      updated_record.update_attributes attributes
+      updated_record.save
+    end
+
   end
 
 end
--- offline_sync_blog_3/example/server/spec/sync_spec.rb
+++ offline_sync_blog_3/example/server/spec/sync_spec.rb
@@ -4,14 +4,15 @@ require "spec_helper"
 describe "Sync" do
 
   it "should add new elements from the client" do
-    client_modified_entities = [
-      {
+    client_modified_entities = [{:post =>
+    {
         :body => "Post Body",
         :guid => "60357A99-9CCD-4D1A-B83A-162E533F915F",
-        :isGloballyDeleted => "0",
-        :lastModified => "1338872968.706092",
-        :title => "post title"
-      }
+        :is_deleted => "0",
+        :updated_at => "1338872968.706092",
+        :title => "post title",
+
+    }}
     ]
     SyncHelper.sync_entities client_modified_entities, 0
     SyncObject.all.should have(1).object
