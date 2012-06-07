4af71bddc8db3069d81aa20722871d2cacb83621
Tyler Casselman <tylerc230@gmail.com>
2012-06-06 19:44:18 -0700

    Test for conflict detection passing
    If the clients version of the entity has a different updated_at
    timestamp than the servers, we know that the server's version
    was modified after the clients was so we have to resolve the conflict
    manually

--- offline_sync_blog_3/example/server/app/helpers/sync_helper.rb
+++ offline_sync_blog_3/example/server/app/helpers/sync_helper.rb
@@ -1,28 +1,44 @@
 module SyncHelper
   MODIFIED_ENTITIES_KEY = :modified_entities
+  CONFLICTED_ENTITIES_KEY = :conflicted_entities
   LAST_SYNC_TIME_KEY = :updated_at
   GUID_KEY = :guid
   CLASS_NAME_KEY = :classname
   def self.sync_entities client_modified_entities, client_last_sync
-    self.sync_client_modifications client_modified_entities
+    conflicts = self.sync_client_modifications client_modified_entities
     response = {}
-    server_modified_entities = SyncObject.modified_since client_last_sync
-    response[MODIFIED_ENTITIES_KEY] = server_modified_entities
+    response[MODIFIED_ENTITIES_KEY] = self.modifications_since_date client_last_sync
+    response[CONFLICTED_ENTITIES_KEY] = conflicts
     response
   end
 
   def self.sync_client_modifications client_modified_entities
+    conflicts = []
     client_modified_entities.each do |entity|
       klass = entity.keys.last
       attributes = entity[klass]
       guid = attributes[GUID_KEY]
       updated_record = SyncObject.find_by_guid guid
       if updated_record.nil?
-        updated_record = klass.to_s.capitalize.constantize.new
+        updated_record = klass.to_s.capitalize.constantize.new attributes
+      else
+        conflict_detected = self.detect_conflict client_last_updated, updated_record
+        if conflict_detected
+          conflicts << updated_record
+        else
+          updated_record.update_attributes attributes
+        end
       end
-      updated_record.update_attributes attributes
       updated_record.save
     end
+  end
+
+  def self.detect_conflict client_last_updated_at, server_entity
+    client_last_updated_at != server_entity.modified_at
+  end
+
+  def self.modifications_since_date client_last_sync
+    return SyncObject.modified_since client_last_sync
   end
 
 end
--- offline_sync_blog_3/example/server/spec/sync_spec.rb
+++ offline_sync_blog_3/example/server/spec/sync_spec.rb
@@ -4,24 +4,15 @@ require "spec_helper"
 describe "Sync" do
 
   it "should add new elements from the client" do
-    client_modified_entities = [{:post =>
-    {
-        :body => "Post Body",
-        :guid => "60357A99-9CCD-4D1A-B83A-162E533F915F",
-        :is_deleted => "0",
-        :updated_at => "1338872968.706092",
-        :title => "post title",
-
-    }}
-    ]
+    client_modified_entities = [entity_with_guid( "60357A99-9CCD-4D1A-B83A-162E533F915F")]
     SyncHelper.sync_entities client_modified_entities, 0
     SyncObject.all.should have(1).object
 
   end
 
   it "should return all modified objects since date" do
-    modified_entity = FactoryGirl.create(:post, {:updated_at => 1.day.ago})
-    FactoryGirl.create(:post, {:updated_at => 2.day.ago})
+    modified_entity = Factory(:post, {:updated_at => 1.day.ago})
+    Factory(:post, {:updated_at => 2.day.ago})
     response = SyncHelper.sync_entities [], 1.day.ago - 60
     entities_modified_since_date = response[SyncHelper::MODIFIED_ENTITIES_KEY]
 
@@ -29,4 +20,28 @@ describe "Sync" do
     entities_modified_since_date.last.should eq(modified_entity)
 
   end
+
+  it "should detect conflicts when they occur" do
+    shared_guid = "60357A99-9CCD-4D1A-B83A-162E533F915F",
+    Factory(:post, {:updated_at => 1.day.ago, :guid => shared_guid})
+    client_entities = [entity_with_guid(shared_guid)]
+    response = SyncHelper.sync_entities client_entities, 0
+    conflicted_entities = response[SyncHelper::CONFLICTED_ENTITIES_KEY]
+    conflicted_entities.should have(1).conflicted_entity
+  end
+
+end
+
+def entity_with_guid guid
+  {:post =>
+     {
+       :body => "Post Body",
+       :guid => guid,
+       :is_deleted => "0",
+       :updated_at => "1338872968.706092",
+       :title => "post title",
+
+     }}
+
+
 end
